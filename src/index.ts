f550c17f4c3018ca1b30509ddda45cd6b11c68f6
Gabriel C <gabriel.cioropina@altex.ro>
2019-08-07 12:47:25 +0300

    log reason on unhandled rejection

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ process
     });
   })
   .on("unhandledRejection", (reason, p) => {
-    Logger.error("Unhandled Rejection at Promise");
+    Logger.error("Unhandled Rejection at Promise", reason);
     Server.recycle().then(err => {
       Logger.info(`Server recycled`);
       process.exit(err ? 1 : 0);
